0973ec3102c01e08443b4eaff627ec9233776ac2
nsheff <nsheff@users.noreply.github.com>
2018-04-05 16:53:06 -0400

    add ignores, test skeleton

---
+++ tests/testthat/test_pepr.R
@@ -0,0 +1,25 @@
+library(pepr)
+
+# p = Project(file = "hello")
+# p
+# p@file
+# p@samples
+# p@config
+# config(p)
+# go()
+
+# p
+
+# p@file
+
+# Project()
+
+# # equivalent:
+# cfg = yaml::yaml.load_file("code/microtest/config/microtest_config.yaml")
+# cfg
+
+# refreshPackage('pepr')
+# library('pepr')
+# p = Project(file = "~/code/microtest/config/microtest_config.yaml")
+
+# samples(p)
