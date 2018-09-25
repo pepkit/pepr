35e1f84cc81c95afc0dae96faeec5601290042e5
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-09-25 16:01:38 -0400

    Redisign the interface class completely, it is the extension of the SummarizedExperiment now

--- R/interface_class.R
+++ R/interface_class.R
@@ -1,17 +1,16 @@
 
 
-
-
 #' A class representing a Portable Encapsulated Project and Summarized Experiment objects interface
 #' This is a helper that creates the Interface with empty Project and SummarizedExperiment objects included
 #'
 #' @param pep an object of \linkS4class{Project} class
-#' @param  se and object of \linkS4class{SummarizedExperiment} class
+#' @inheritParams  SummarizedExperiment::SummarizedExperiment 
 #'
 #' @export Interface
-Interface = function(pep = NULL,
-                     se = NULL) {
-  new("Interface", pep = pep, se = se)
+Interface = function(pep = NULL, ...) {
+  # The three dots pass the arguments
+  rse=SummarizedExperiment(...)
+  new("Interface", rse, pep=pep)
 }
 
 #' A class representing a Portable Encapsulated Project and Summarized Experiment objects interface
@@ -21,31 +20,23 @@ Interface = function(pep = NULL,
 #' Additionally, this class privides an interfece that connects them.
 #'
 #' @slot pep an object of \linkS4class{Project} class
-#' @slot se and object of \linkS4class{SummarizedExperiment} class
 #'
 #' @exportClass Interface
 setClass(
   "Interface",
-  slots = c(pep = "Project", se = "SummarizedExperiment"),
+  slots = c(pep = "Project"),
   # Inherits from Project and SummarizedExperiment objects
-  contains = c("Project", "SummarizedExperiment")
+  contains ="RangedSummarizedExperiment"
 )
 
 setMethod(
   f = "initialize",
   signature = "Interface",
-  definition = function(.Object, pep, se) {
-    # Functional object is created even if neither Project nor SummarizedExperiment object is provided
-    if (missing(pep)) {
-      .Object@pep = new("Project")
-    } else{
-      .Object@pep = pep
-    }
-    if (missing(se)) {
-      .Object@se = new("SummarizedExperiment")
-    } else{
-      .Object@se = se
-    }
+  definition = function(.Object, pep, ...) {
+    # if no pep argument provided an empty Project object is created
+    if (is.null(pep)) pep = new("Project")
+    # call initialize method of SummarizedExperiment class
+    .Object=callNextMethod()
     return(.Object)
   }
 )
@@ -55,11 +46,10 @@ setMethod(
   signature = "Interface",
   definition = function(.Object) {
     # TODO: Need to make the look consistent
-    message("Project:")
+    cat("\n")
+    callNextMethod(.Object)
+    cat("\n")
     show(.Object@pep)
-    message("")
-    cat("SummarizedExperiment:\n")
-    show(.Object@se)
     invisible(NULL)
   }
 )
@@ -68,28 +58,28 @@ setMethod(
 setGeneric("getColData", function(.Object, ...)
   standardGeneric("getColData"))
 
-#' Get colData from Project object
+#' Get colData from the Project object (PEP)
 #'
-#' This method copies info about samples from \linkS4class{Project} object to \linkS4class{SummarizedExperiment} object.
+#' This method copies info about samples from \linkS4class{Project} object to \code{colData} slot on the \code{Interface} object.
 #'
 #' @param .Object An object of Interface class
 #'
-#' @return .Object An object of Interface class. The colData attribute of \linkS4class{SummarizedExperiment} is derived from samples attribute of \linkS4class{Project}
+#' @return .Object An object of \code{Interface} class. The colData slot is derived from samples attribute of \linkS4class{Project}
 #' @export getColData
 setMethod(
   "getColData",
   signature = "Interface",
   definition = function(.Object) {
     # Check the compatibility - dimensions
-    if (NCOL(.Object@se) != NROW(.Object@pep@samples))
+    if (NCOL(.Object) != NROW(.Object@pep@samples))
       stop(
         "The number of rows in Project samples (",
         NROW(.Object@pep@samples),
         ") and SummarizedExperiment colData columns (",
-        NCOL(.Object@se),
+        NCOL(.Object),
         ") are not equal."
       )
-    .Object@se@colData = DataFrame(.Object@pep@samples)
+    colData(.Object) = DataFrame(.Object@pep@samples)
     return(.Object)
   }
 )
@@ -98,27 +88,30 @@ setMethod(
 setGeneric("getMetadata", function(.Object, ...)
   standardGeneric("getMetadata"))
 
-#' Get metadata from Project object
+#' Get metadata from the Project object (PEP)
 #'
-#' This method copies metadata from \linkS4class{Project} object to \linkS4class{MIAME} in the \linkS4class{SummarizedExperiment} object.
+#' This method copies metadata from \linkS4class{Project} object to \linkS4class{MIAME} in the \code{Interface} object.
 #'
 #' @param .Object An object of Interface class
 #'
-#' @return .Object An object of Interface class. The metadata attribute of \linkS4class{SummarizedExperiment} is enriched with data from \linkS4class{Project} object as an additional \code{PEP config file} field in the MIAME object.
+#' @return .Object An object of Interface class. The metadata slot of \code{Interface} is enriched with data from \linkS4class{Project} object as an additional \code{PEP config file} field in the MIAME object.
 #' @export getMetadata
 setMethod(
   "getMetadata",
   signature = "Interface",
   definition = function(.Object) {
     # Check if MIAME object exists in the metadata section of SummarizedExperiment, create new if not
-    if (is.list(.Object@se@metadata) &
-        length(.Object@se@metadata) == 0) {
-      .Object@se@metadata = MIAME(name = "Experiment data",
-                                  other = list(`PEP config file` = .Object@pep@file))
+    if (is.list(metadata(.Object)) &
+        length(metadata(.Object)) == 0) {
+      metadata(.Object) = list(MIAME(other = list(`PEP config file` = .Object@pep@file)))
       # Otherwise append existing one with the PEP file info
     } else{
-      .Object@se@metadata = lapply(.Object@se@metadata, function(x) {
-        x = combine(x, MIAME(other = list(`PEP config file` = .Object@pep@file)))
+      metadata(.Object) = lapply(metadata(.Object), function(x) {
+        x = tryCatch({
+          combine(x, MIAME(other = list(`PEP config file` = .Object@pep@file)))
+        }, error = function(e) {
+          stop("The metadata section of the Interface object is not a MIAME object.")
+        })
         return(x)
       })
     }
