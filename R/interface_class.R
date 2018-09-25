c7c4d3cffad2b521e21ba8ea093da2ba2614a1b7
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-09-25 12:27:43 -0400

    Add docs and mwthods to interface class

--- R/interface_class.R
+++ R/interface_class.R
@@ -1,14 +1,33 @@
 
 
 
+
+#' A class representing a Portable Encapsulated Project and Summarized Experiment objects interface
+#' This is a helper that creates the Interface with empty Project and SummarizedExperiment objects included
+#'
+#' @param pep an object of \linkS4class{Project} class
+#' @param  se and object of \linkS4class{SummarizedExperiment} class
+#'
+#' @export Interface
 Interface = function(pep = NULL,
                      se = NULL) {
   new("Interface", pep = pep, se = se)
 }
 
+#' A class representing a Portable Encapsulated Project and Summarized Experiment objects interface
+#'
+#' Provides a representation and functions to access project
+#' configuration and sample annotation values for a PEP as well as functions concerning experimental results organization provided by the \linkS4class{SummarizedExperiment} class.
+#' Additionally, this class privides an interfece that connects them.
+#'
+#' @slot pep an object of \linkS4class{Project} class
+#' @slot se and object of \linkS4class{SummarizedExperiment} class
+#'
+#' @exportClass Interface
 setClass(
   "Interface",
   slots = c(pep = "Project", se = "SummarizedExperiment"),
+  # Inherits from Project and SummarizedExperiment objects
   contains = c("Project", "SummarizedExperiment")
 )
 
@@ -16,6 +35,7 @@ setMethod(
   f = "initialize",
   signature = "Interface",
   definition = function(.Object, pep, se) {
+    # Functional object is created even if neither Project nor SummarizedExperiment object is provided
     if (missing(pep)) {
       .Object@pep = new("Project")
     } else{
@@ -35,11 +55,10 @@ setMethod(
   signature = "Interface",
   definition = function(.Object) {
     # TODO: Need to make the look consistent
-    message("")
     message("Project:")
     show(.Object@pep)
     message("")
-    message("SummarizedExperiment:")
+    cat("SummarizedExperiment:\n")
     show(.Object@se)
     invisible(NULL)
   }
@@ -49,7 +68,14 @@ setMethod(
 setGeneric("getColData", function(.Object, ...)
   standardGeneric("getColData"))
 
-#' @export
+#' Get colData from Project object
+#'
+#' This method copies info about samples from \linkS4class{Project} object to \linkS4class{SummarizedExperiment} object.
+#'
+#' @param .Object An object of Interface class
+#'
+#' @return .Object An object of Interface class. The colData attribute of \linkS4class{SummarizedExperiment} is derived from samples attribute of \linkS4class{Project}
+#' @export getColData
 setMethod(
   "getColData",
   signature = "Interface",
@@ -64,6 +90,38 @@ setMethod(
         ") are not equal."
       )
     .Object@se@colData = DataFrame(.Object@pep@samples)
+    return(.Object)
+  }
+)
+
+
+setGeneric("getMetadata", function(.Object, ...)
+  standardGeneric("getMetadata"))
+
+#' Get metadata from Project object
+#'
+#' This method copies metadata from \linkS4class{Project} object to \linkS4class{MIAME} in the \linkS4class{SummarizedExperiment} object.
+#'
+#' @param .Object An object of Interface class
+#'
+#' @return .Object An object of Interface class. The metadata attribute of \linkS4class{SummarizedExperiment} is enriched with data from \linkS4class{Project} object as an additional \code{PEP config file} field in the MIAME object.
+#' @export getMetadata
+setMethod(
+  "getMetadata",
+  signature = "Interface",
+  definition = function(.Object) {
+    # Check if MIAME object exists in the metadata section of SummarizedExperiment, create new if not
+    if (is.list(.Object@se@metadata) &
+        length(.Object@se@metadata) == 0) {
+      .Object@se@metadata = MIAME(name = "Experiment data",
+                                  other = list(`PEP config file` = .Object@pep@file))
+      # Otherwise append existing one with the PEP file info
+    } else{
+      .Object@se@metadata = lapply(.Object@se@metadata, function(x) {
+        x = combine(x, MIAME(other = list(`PEP config file` = .Object@pep@file)))
+        return(x)
+      })
+    }
     return(.Object)
   }
 )
