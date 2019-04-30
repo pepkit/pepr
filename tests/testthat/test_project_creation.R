500483faaaac052595edaf616c8e9258e2e6fa38
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-04-30 13:26:12 -0400

    more unit tests

--- tests/testthat/test_project_creation.R
+++ tests/testthat/test_project_creation.R
@@ -22,12 +22,21 @@ yaml = yaml.load_file(system.file(
   "project_config.yaml",
   package = "pepr"
 ))
-p_yaml=Project(system.file(
+
+pYaml=Project(system.file(
   "extdata",
   paste0("example_peps-",branch),
   "example_subprojects2",
   "project_config.yaml",
   package = "pepr"
+))
+
+pBioc=Project(system.file(
+    "extdata",
+    paste0("example_peps-",branch),
+    "example_BiocProject",
+    "project_config.yaml",
+    package = "pepr"
 ))
 
 # tests -------------------------------------------------------------------
@@ -70,6 +79,14 @@ test_that("Project creates an object of class Project", {
 test_that("Project (loadConfig) produces a proper config file.
           YAML read config has to consist of list elements of the same length
           as the config processed with the Project constructor", {
-            expect_equal(unlist(lapply(config(p_yaml)$metadata,length)),
+            expect_equal(unlist(lapply(config(pYaml)$metadata,length)),
                          unlist(lapply(yaml$metadata,length)))
             })
+
+test_that("Project successfully loads a config with bioconductor section", {
+    expect_true(is(pBioc,"Project"))
+})
+
+test_that("Project succesfully activates subproject at initialization", {
+    expect_equal(NROW(samples(Project(pYaml@file, subproject = "noFrog"))), 4)
+})
