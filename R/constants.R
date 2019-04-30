3d6790beabf8e96b926221330477898ad6807df5
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-04-30 11:41:02 -0400

    remove constatnts that were transferred to BiocProject

--- R/constants.R
+++ R/constants.R
@@ -2,7 +2,3 @@ kOldPipelinesSection = "pipelines_dir"
 kRelativeToOutputDirMetadataSections = c("results_subdir", "submission_subdir")
 
 kRelativeToConfigSections = c("metadata", "pipeline_config")
-METADATA_NAME = "metadata"
-PIP_IFACE_NAME = "pipeline_interfaces"
-PIP_IFACE_SECTION = c(METADATA_NAME, PIP_IFACE_NAME)
-
