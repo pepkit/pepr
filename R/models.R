4fc92937b595051143c396a57ff5981cf57649a8
nsheff <nsheff@users.noreply.github.com>
2019-11-22 09:41:04 -0500

    Don't fail on extra subsamples; just don't use them

--- R/models.R
+++ R/models.R
@@ -535,12 +535,13 @@ setMethod(
       whichColSamples = which(names(samples) == colName)
       whichRowSamples = which(samples$sample_name == iName)
       if(length(whichRowSamples) < 1){
-        stop("No samples named '", iName, "'")
+        warning("No samples named '", iName, "'")
+      } else {
+        # Inserting element(s) into the list
+        colList[[whichRowSamples]] = subTable[[colName]]
+        # Inserting the list as a column in the data.frame
+        samples[[colName]] = colList
       }
-      # Inserting element(s) into the list
-      colList[[whichRowSamples]] = subTable[[colName]]
-      # Inserting the list as a column in the data.frame
-      samples[[colName]] = colList
     }
   }
   samples[is.na(samples)] = ""
