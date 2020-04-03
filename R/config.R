027c58edd2e20bef3f3963f5d304e77b5d39cfb8
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-04-03 16:24:01 -0400

    support multi-value looper subsections

--- R/config.R
+++ R/config.R
@@ -204,9 +204,12 @@ setMethod(
   if(!is.null(cfg_data[[CFG_LOOPER_KEY]])){
     nms = names(cfg_data[[CFG_LOOPER_KEY]])
     for(i in seq_along(cfg_data[[CFG_LOOPER_KEY]])){
-      path = gsub("\\./","", cfg_data[[CFG_LOOPER_KEY]][[nms[i]]])
-      cfg_data[[CFG_LOOPER_KEY]][[nms[i]]] = 
-        .makeAbsPath(path, parent=dirname(filename))
+      paths = cfg_data[[CFG_LOOPER_KEY]][[nms[i]]]
+      print(paths)
+      for(j in seq_along(paths)){
+        cfg_data[[CFG_LOOPER_KEY]][[nms[i]]][j] = 
+          .makeAbsPath(paths[j], parent=dirname(filename))
+      }
     }
   }
   cfg_data$name = .inferProjectName(cfg_data, filename)
